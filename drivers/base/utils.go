2ea6ca0cfea2d7aa25c9be0901eca3d3efbb938b
hash-data <ankit@datazip.io>
2025-02-18 18:21:20 +0530

    chore: adding some formatting

--- drivers/base/utils.go
+++ drivers/base/utils.go
@@ -7,11 +7,11 @@ import (
 )
 
 func RetryOnBackoff(attempts int, sleep time.Duration, f func() error) (err error) {
-	for cur := 1; cur <= attempts; cur++ {
+	for cur := 0; cur < attempts; cur++ {
 		if err = f(); err == nil {
 			return nil
 		}
-		logger.Infof("Retrying after %f seconds due to err: %s", sleep.Seconds(), err)
+		logger.Infof("retry attempt[%d], retrying after %.2f seconds due to err: %s", cur+1, sleep.Seconds(), err)
 		time.Sleep(sleep)
 		sleep = sleep * 2
 	}
